c62c3485beeee3e1dc048c7a0d9031408d21d2da
Samuel Rince <s@rince.me>
2024-03-05 12:02:23 +0100

    refactor: remove __main__.py

--- genai_impact/__main__.py
+++
@@ -1,9 +0,0 @@
-from genai_impact.client import OpenAI
-
-client = OpenAI()
-
-response = client.chat.completions.create(
-    model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Hello World!"}]
-)
-
-print(response.impacts)
