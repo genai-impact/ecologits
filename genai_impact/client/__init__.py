cf9eb155fbbeb45233eae42df5384198f4ef2220
Samuel Rince <s@rince.me>
2024-03-07 11:35:16 +0100

    feat: add anthropic client to module inits

--- genai_impact/client/__init__.py
+++ genai_impact/client/__init__.py
@@ -1,4 +1,5 @@
+from .anthropic_wrapper import Anthropic
 from .mistralai_wrapper import MistralClient
 from .openai_wrapper import OpenAI
 
-__all__ = ["OpenAI", "MistralClient"]
+__all__ = ["OpenAI", "MistralClient", "Anthropic"]
