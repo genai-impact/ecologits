c4e070e6151de78d3e12e9bcbef80795a665f451
Samuel Rince <s@rince.me>
2024-03-19 23:27:15 +0100

    feat: use model repository in each tracer

--- genai_impact/compute_impacts.py
+++ genai_impact/compute_impacts.py
@@ -1,15 +1,31 @@
+from typing import Union
+
 from pydantic import BaseModel
 
-ENERGY_PROFILE = 1.17e-4
+GPU_ENERGY_ALPHA = 8.91e-5
+GPU_ENERGY_BETA = 1.43e-3
 
 
 class Impacts(BaseModel):
     energy: float
+    energy_range: tuple[float, float]
     energy_unit: str = "Wh"
 
 
 def compute_llm_impact(
-    model_parameter_count: float,
+    model_parameter_count: Union[float, tuple[float, float]],
     output_token_count: int,
 ) -> Impacts:
-    return Impacts(energy=ENERGY_PROFILE * model_parameter_count * output_token_count)
+    if isinstance(model_parameter_count, (tuple, list)):
+        # TODO: check tuple or list validity (min, max)
+        energy_min = output_token_count * (GPU_ENERGY_ALPHA * model_parameter_count[0] + GPU_ENERGY_BETA)
+        energy_max = output_token_count * (GPU_ENERGY_ALPHA * model_parameter_count[1] + GPU_ENERGY_BETA)
+        energy_avg = (energy_min + energy_max) / 2
+    else:
+        energy_avg = output_token_count * (GPU_ENERGY_ALPHA * model_parameter_count + GPU_ENERGY_BETA)
+        energy_min = energy_avg
+        energy_max = energy_avg
+    return Impacts(
+        energy=energy_avg,
+        energy_range=(energy_min, energy_max)
+    )
