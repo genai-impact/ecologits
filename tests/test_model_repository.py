70168c1425feed3148fb5d0194513e128a7a7670
Samuel Rince <s@rince.me>
2024-03-19 23:16:15 +0100

    test: add some unit tests for model repository

---
+++ tests/test_model_repository.py
@@ -0,0 +1,24 @@
+from genai_impact.model_repository import ModelRepository, Model
+
+
+def test_create_model_repository_default():
+    models = ModelRepository.from_csv()
+    assert isinstance(models, ModelRepository)
+    assert models.find_model(provider="openai", model_name="gpt-3.5-turbo") is not None
+
+
+def test_create_model_repository_from_scratch():
+    models = ModelRepository([
+        Model(provider="provider-test", name="model-test")
+    ])
+    assert models.find_model(provider="provider-test", model_name="model-test")
+
+
+def test_find_unknown_provider():
+    models = ModelRepository.from_csv()
+    assert models.find_model(provider="provider-test", model_name="gpt-3.5-turbo") is None
+
+
+def test_find_unknown_model_name():
+    models = ModelRepository.from_csv()
+    assert models.find_model(provider="openai", model_name="model-test") is None
