38e9400601027b45c0fe9f87c9c8d9b8071c7820
luc <luc.jp.berton@test.com>
2024-03-27 17:22:20 +0100

    ongoing not functionnal anthropic streams

--- tests/test_anthropic.py
+++ tests/test_anthropic.py
@@ -25,3 +25,18 @@ async def test_anthropic_async_chat(tracer_init):
     )
     assert len(response.content) > 0
     assert response.impacts.energy > 0
+
+@pytest.mark.vcr
+def test_anthropic_stream_chat(tracer_init):
+    client = Anthropic()
+    with client.messages.stream(
+        max_tokens=1024,
+        messages=[{"role": "user", "content": "Hello"}],
+        model="claude-3-haiku-20240307",
+    ) as stream:
+        text_response = ''
+        for text in stream.text_stream:
+            text_response += text
+
+    assert len(text_response) > 0
+    
