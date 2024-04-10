63ed5b3a7af52d205b7b4765e8da3c07f3d2b77a
Adrien Banse <adrien.banse@uclouvain.be>
2024-04-10 18:07:52 +0200

    async anthropic streams

--- genai_impact/tracers/anthropic_tracer.py
+++ genai_impact/tracers/anthropic_tracer.py
@@ -1,5 +1,5 @@
 from types import TracebackType
-from typing import Any, Callable, Generic, Iterator, Optional, TypeVar
+from typing import Any, Callable, Generic, Iterator, Optional, TypeVar, AsyncIterator, Awaitable
 
 from typing_extensions import override
 from wrapt import wrap_function_wrapper
@@ -10,6 +10,7 @@ from genai_impact.model_repository import models
 try:
     from anthropic import Anthropic, AsyncAnthropic
     from anthropic.lib.streaming import MessageStream as _MessageStream
+    from anthropic.lib.streaming import AsyncMessageStream as _AsyncMessageStream
     from anthropic.types import Message as _Message
     from anthropic.types.message_delta_event import MessageDeltaEvent
     from anthropic.types.message_start_event import MessageStartEvent
@@ -18,23 +19,56 @@ except ImportError:
     AsyncAnthropic = object()
     _Message = object()
     _MessageStream = object()
+    _AsyncMessageStream = object()
     MessageDeltaEvent = object()
     MessageStartEvent = object()
 
 
 MessageStreamT = TypeVar("MessageStreamT", bound=_MessageStream)
-
+AsyncMessageStreamT = TypeVar("AsyncMessageStreamT", bound=_AsyncMessageStream)
 
 class Message(_Message):
     impacts: Impacts
 
 
 class MessageStream(_MessageStream):
+    impacts: Impacts
+
     @override
     def __stream_text__(self) -> Iterator[str]:
         output_tokens = 0
         model = None
         for chunk in self:
+            if type(chunk) is MessageStartEvent:
+                message = chunk.message
+                model = models.find_model(provider="anthropic", model_name=message.model)
+                output_tokens += message.usage.output_tokens
+            elif type(chunk) is MessageDeltaEvent:
+                output_tokens += chunk.usage.output_tokens
+            elif chunk.type == "content_block_delta" and chunk.delta.type == "text_delta":
+                yield chunk.delta.text
+        model_size = model.active_parameters or model.active_parameters_range
+        impacts = compute_llm_impact(
+            model_parameter_count=model_size, output_token_count=output_tokens
+        )
+        self.impacts = impacts
+
+    def __init__(self, parent) -> None:     # noqa: ANN001
+        super().__init__(
+            cast_to=parent._cast_to,        # noqa: SLF001
+            response=parent.response,
+            client=parent._client           # noqa: SLF001
+        )
+
+
+class AsyncMessageStream(_AsyncMessageStream):
+    impacts: Impacts
+
+    @override
+    async def __stream_text__(self) -> AsyncIterator[str]:
+        output_tokens = 0
+        model = None
+        async for chunk in self:
             if type(chunk) is MessageStartEvent:
                 message = chunk.message
                 model = models.find_model(provider="anthropic", model_name=message.model)
@@ -77,6 +111,26 @@ class MessageStreamManager(Generic[MessageStreamT]):
             self.__stream.close()
 
 
+class AsyncMessageStreamManager(Generic[AsyncMessageStreamT]):
+    def __init__(self, api_request: Awaitable[AsyncMessageStreamT]) -> None:
+        self.__stream: Optional[AsyncMessageStreamT] = None
+        self.__api_request = api_request
+
+    async def __aenter__(self) -> AsyncMessageStreamT:
+        self.__stream = await self.__api_request
+        self.__stream = AsyncMessageStream(self.__stream)
+        return self.__stream
+
+    async def __aexit__(
+        self,
+        exc_type: Optional[type[BaseException]],
+        exc: Optional[BaseException],
+        exc_tb: Optional[TracebackType]
+    ) -> None:
+        if self.__stream is not None:
+            await self.__stream.close()
+
+
 def compute_impacts_and_return_response(response: Any) -> Message:
     model = models.find_model(provider="anthropic", model_name=response.model)
     if model is None:
@@ -107,9 +161,16 @@ async def anthropic_async_chat_wrapper(
 
 def anthropic_stream_chat_wrapper(
     wrapped: Callable, instance: Anthropic, args: Any, kwargs: Any  # noqa: ARG001
-) -> MessageStreamManager:
+) -> MessageStreamManager[MessageStream]:
     response = wrapped(*args, **kwargs)
     return MessageStreamManager(response._MessageStreamManager__api_request)    # noqa: SLF001
+
+
+def anthropic_async_stream_chat_wrapper(
+    wrapped: Callable, instance: AsyncAnthropic, args: Any, kwargs: Any  # noqa: ARG001
+) -> AsyncMessageStreamManager:
+    response = wrapped(*args, **kwargs)
+    return AsyncMessageStreamManager(response._AsyncMessageStreamManager__api_request)
 
 
 class AnthropicInstrumentor:
@@ -129,6 +190,11 @@ class AnthropicInstrumentor:
                 "module": "anthropic.resources",
                 "name": "Messages.stream",
                 "wrapper": anthropic_stream_chat_wrapper,
+            },
+            {
+                "module": "anthropic.resources",
+                "name": "AsyncMessages.stream",
+                "wrapper": anthropic_async_stream_chat_wrapper,
             },
         ]
 
