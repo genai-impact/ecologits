02fa5b82c12493aea4608eb26d7e35fcd63ebac4
Samuel Rince <s@rince.me>
2024-04-10 14:25:59 +0200

    test: record test for anthropic stream

--- tests/test_anthropic.py
+++ tests/test_anthropic.py
@@ -27,18 +27,19 @@ async def test_anthropic_async_chat(tracer_init):
     assert response.impacts.energy > 0
 
 
-@pytest.mark.skip
 @pytest.mark.vcr
 def test_anthropic_stream_chat(tracer_init):
     client = Anthropic()
+
+    text_response = ''
     with client.messages.stream(
-        max_tokens=1024,
-        messages=[{"role": "user", "content": "Hello"}],
+        max_tokens=100,
+        messages=[{"role": "user", "content": "Hello World!"}],
         model="claude-3-haiku-20240307",
     ) as stream:
-        text_response = ''
         for text in stream.text_stream:
             text_response += text
+        assert stream.impacts.energy >= 0
 
     assert len(text_response) > 0
     
