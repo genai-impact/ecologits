7211aafcb60a1cb08327aa56469c0065f55660b5
Adrien Banse <adrien.banse@uclouvain.be>
2024-04-10 18:35:18 +0200

    async anthropic stream test

--- tests/test_anthropic.py
+++ tests/test_anthropic.py
@@ -42,4 +42,21 @@ def test_anthropic_stream_chat(tracer_init):
         assert stream.impacts.energy >= 0
 
     assert len(text_response) > 0
+
+
+@pytest.mark.vcr
+@pytest.mark.asyncio
+async def test_openai_async_stream_chat(tracer_init):
+    client = AsyncAnthropic()
+
+    text_response = ''
+    async with client.messages.stream(
+        max_tokens=100,
+        messages=[{"role": "user", "content": "Hello World!"}],
+        model="claude-3-haiku-20240307",
+    ) as stream:
+        async for text in stream.text_stream:
+            text_response += text
+        assert stream.impacts.energy >= 0
     
+    assert len(text_response) > 0
