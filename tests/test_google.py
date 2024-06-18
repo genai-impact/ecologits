f8ee41e04cfcaf93923470cc20f832dfd4c82104
Adrien Banse <adrien.banse@uclouvain.be>
2024-06-18 16:06:43 +0200

    fix: pytest

--- tests/test_google.py
+++ tests/test_google.py
@@ -4,7 +4,6 @@ import google.generativeai as genai
 
 @pytest.mark.vcr
 def test_google_chat(tracer_init):
-    genai.configure(api_key="") #TODO add a key or record VCR
     model = genai.GenerativeModel("gemini-1.5-flash")
     response = model.generate_content("Write a story about a magic backpack.")
     assert len(response.text) > 0
