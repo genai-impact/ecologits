4113ee4f47f30512eb350117bf1bcb82da9c734c
PETARD Nina <23741953+NP4567-dev@users.noreply.github.com>
2024-08-01 15:48:20 +0200

    fix: fix gpt-4-turbo matching gpt-4

--- tests/test_model_repository.py
+++ tests/test_model_repository.py
@@ -1,3 +1,4 @@
+import pytest
 from ecologits.model_repository import ModelRepository, Model
 
 
@@ -16,6 +17,20 @@ def test_create_model_repository_from_scratch():
 def test_find_azure_openai_model():
     models = ModelRepository.from_csv()
     assert models.find_model(provider="openai", model_name="gpt-35-turbo").name =="gpt-3.5-turbo"
+
+
+@pytest.mark.parametrize("input_provider,input_name, expected_name",
+                         [("openai", "gpt-35-turbo", "gpt-3.5-turbo"),
+                          ("openai", "gpt-3.5-turbo", "gpt-3.5-turbo"),
+                          ("openai", "gpt-4", "gpt-4"),
+                          ("openai", "gpt-4-turbo", "gpt-4-turbo"),
+                          ("mistralai", "mistral-small-2312", "mistral-small-2312"),
+                          ("mistralai", "mistral-small-notarealversion", "mistral-small"),
+                          ("google", "gemini-1.5-flash", "gemini-1.5-flash"),
+                         ])
+def test_nominal_matching_cases(input_provider, input_name, expected_name):
+    models = ModelRepository.from_csv()
+    assert models.find_model(provider=input_provider, model_name=input_name).name ==expected_name
 
 
 def test_find_unknown_provider():
