15a40e087c892cd3385739791aaca16f098c6197
Nina P. <23741953+NP4567-dev@users.noreply.github.com>
2024-08-23 11:59:26 +0200

    Feature/azure tests (#3)
    * revert: roll back find_model and deleted related tests, updated data csv
    
    * feat: review changes
    
    * feat: added tests and slight fix
    
    * fix: removed extra csv rows
    
    * fix: rolled back env specific changes
    
    * fix: rolled back env specific changes

--- tests/test_openai.py
+++ tests/test_openai.py
@@ -1,6 +1,6 @@
+import os
 import pytest
-from openai import OpenAI, AsyncOpenAI
-
+from openai import OpenAI, AsyncOpenAI, AzureOpenAI, AsyncAzureOpenAI
 
 @pytest.mark.vcr
 def test_openai_chat(tracer_init):
@@ -37,14 +37,39 @@ def test_openai_stream_chat(tracer_init):
         assert chunk.impacts.energy.value >= 0
 
 
+
+
+@pytest.mark.vcr
+def test_azure_openai_chat(tracer_init):
+    client = AzureOpenAI(azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT")
+                        )
+    response = client.chat.completions.create(        
+        messages=[{"role": "user", "content": "Hello World!"}],
+        model=os.getenv("AZURE_MODEL_DEPLOYMENT"),
+    )
+    assert len(response.choices) > 0
+    assert response.impacts.energy.value > 0
+    
 @pytest.mark.vcr
 @pytest.mark.asyncio
-async def test_openai_async_stream_chat(tracer_init):
-    client = AsyncOpenAI()
-    stream = await client.chat.completions.create(
-        model="gpt-3.5-turbo",
+async def test_azure_openai_async_chat(tracer_init):
+    client = AsyncAzureOpenAI(azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"))
+    response = await client.chat.completions.create(
+         model=os.getenv("AZURE_MODEL_DEPLOYMENT"),
+        messages=[{"role": "user", "content": "Hello World!"}]
+    )
+    assert len(response.choices) > 0
+    assert response.impacts.energy.value > 0
+
+@pytest.mark.vcr
+def test_azure_openai_stream_chat(tracer_init):
+    client = AzureOpenAI(azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+            )
+    stream = client.chat.completions.create(
+        model=os.getenv("AZURE_MODEL_DEPLOYMENT"),
         messages=[{"role": "user", "content": "Hello World!"}],
         stream=True
     )
-    async for chunk in stream:
+    for chunk in stream:
         assert chunk.impacts.energy.value >= 0
+        
