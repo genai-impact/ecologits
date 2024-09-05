a827a978c878fa9600a57650a69c8c957c6fb07b
Adrien Banse <adrien.banse@uclouvain.be>
2024-09-05 16:35:04 +0200

    test: tests for initialization with different parameters

--- tests/test_tracer.py
+++ tests/test_tracer.py
@@ -1,19 +1,22 @@
 import pytest
 from ecologits import EcoLogits
-from ecologits.exceptions import EcoLogitsError
 
 from openai import OpenAI
 from anthropic import Anthropic
 
-
-@pytest.mark.skip(reason="Double init does not raise an error anymore, but we should test that it works correctly.")
+@pytest.mark.vcr
 def test_double_init(tracer_init):
-    with pytest.raises(EcoLogitsError) as e:
-        EcoLogits.init()   # Second initialization
-
+    EcoLogits.init() # second init
+    openai_client = OpenAI()
+    openai_response = openai_client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": "Hello World!"}]
+    )
+    assert len(openai_response.choices) > 0
+    assert hasattr(openai_response, "impacts")
 
 @pytest.mark.skip(reason="Must implement un-instrument behavior first.")
-def test_init_openai_only():
+def test_init_with_different_providers():
     EcoLogits.init(providers=["openai"])
 
     openai_client = OpenAI()
@@ -32,6 +35,23 @@ def test_init_openai_only():
     )
     assert len(anthropic_response.content) > 0
     assert not hasattr(anthropic_response, "impacts")
+
+    EcoLogits.init(providers=["anthropic"]) # adds anthropic
+
+    openai_response = openai_client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": "Hello World!"}]
+    )
+    assert len(openai_response.choices) > 0
+    assert hasattr(openai_response, "impacts")
+
+    anthropic_response = anthropic_client.messages.create(
+        max_tokens=100,
+        messages=[{"role": "user", "content": "Hello World!"}],
+        model="claude-3-5-sonnet-20240620",
+    )
+    assert len(anthropic_response.content) > 0
+    assert hasattr(anthropic_response, "impacts")
 
 @pytest.mark.vcr
 def test_init_with_different_mixes():
