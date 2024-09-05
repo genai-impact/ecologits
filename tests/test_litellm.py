63188455e8328111cd598c86fcc31cea25c83660
Adrien Banse <adrien.banse@uclouvain.be>
2024-09-05 17:16:51 +0200

    fix: claude instead of mistral for litellm

--- tests/test_litellm.py
+++ tests/test_litellm.py
@@ -5,7 +5,7 @@ import litellm
 @pytest.mark.vcr
 def test_litellm_chat(tracer_init):
     response = litellm.completion(
-        messages=[{"role": "user", "content": "Hello World!"}], model="mistral/mistral-small-latest"
+        messages=[{"role": "user", "content": "Hello World!"}], model="claude-3-5-sonnet-20240620"
     )
     assert len(response.choices) > 0
     assert response.impacts.energy.value > 0
@@ -24,7 +24,7 @@ async def test_litellm_async_chat(tracer_init):
 @pytest.mark.vcr
 def test_litellm_stream_chat(tracer_init):
     stream = litellm.completion(
-        messages=[{"role": "user", "content": "Hello World!"}], model="mistral/mistral-small-latest", stream=True
+        messages=[{"role": "user", "content": "Hello World!"}], model="claude-3-5-sonnet-20240620", stream=True
     )
     for chunk in stream:
         assert chunk.impacts.energy.value >= 0
