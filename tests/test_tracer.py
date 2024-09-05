d57601e8d41b0510ac20b9ed85014b15e6c9420d
Adrien Banse <adrien.banse@uclouvain.be>
2024-09-05 15:47:43 +0200

    test: add zone test + record

--- tests/test_tracer.py
+++ tests/test_tracer.py
@@ -2,6 +2,9 @@ import pytest
 from ecologits import EcoLogits
 from ecologits.exceptions import EcoLogitsError
 
+from openai import OpenAI
+from anthropic import Anthropic
+
 
 @pytest.mark.skip(reason="Double init does not raise an error anymore, but we should test that it works correctly.")
 def test_double_init(tracer_init):
@@ -11,10 +14,7 @@ def test_double_init(tracer_init):
 
 @pytest.mark.skip(reason="Must implement un-instrument behavior first.")
 def test_init_openai_only():
-    from openai import OpenAI
-    from anthropic import Anthropic
-
-    EcoLogits.init("openai")
+    EcoLogits.init(providers=["openai"])
 
     openai_client = OpenAI()
     openai_response = openai_client.chat.completions.create(
@@ -32,3 +32,34 @@ def test_init_openai_only():
     )
     assert len(anthropic_response.content) > 0
     assert not hasattr(anthropic_response, "impacts")
+
+@pytest.mark.vcr
+def test_init_with_different_mixes():
+    client = Anthropic()
+
+    EcoLogits.init(electricity_mix_zone="FRA")
+    message_under_french_mix = client.messages.create(
+        max_tokens=1024,
+        messages=[
+            {
+                "role": "user",
+                "content": "Hello, Claude",
+            }
+        ],
+        model="claude-3-opus-20240229",
+    )
+
+    EcoLogits.init(electricity_mix_zone="BEL")
+    message_under_belgian_mix = client.messages.create(
+        max_tokens=1024,
+        messages=[
+            {
+                "role": "user",
+                "content": "Hello, Claude",
+            }
+        ],
+        model="claude-3-opus-20240229",
+    )
+
+    assert message_under_belgian_mix.impacts.energy.value.min != message_under_french_mix.impacts.energy.value.min
+    
