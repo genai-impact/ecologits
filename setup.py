837e77696ae310c5e0246581d4af7f229d02d997
Samuel Rince <s@rince.me>
2025-05-08 18:14:20 +0200

    chore: remove setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="ecologits",
-    version="0.1.0",
-    packages=find_packages(include=["ecologits", "ecologits.*"]),
-    include_package_data=True,
-    install_requires=[],
-)
