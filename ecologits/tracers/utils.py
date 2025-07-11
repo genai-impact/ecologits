d9bb38fa46185c108b56d7f07fb235dc1264dca8
Chieh <hsuchieh1010@gmail.com>
2025-07-11 23:36:23 +0200

    added water consumption calculations.

--- ecologits/tracers/utils.py
+++ ecologits/tracers/utils.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 
 from ecologits.electricity_mix_repository import electricity_mixes
 from ecologits.impacts.llm import compute_llm_impacts
-from ecologits.impacts.modeling import GWP, PE, ADPe, Embodied, Energy, Usage
+from ecologits.impacts.modeling import GWP, PE, ADPe, Embodied, Energy, Water, Usage
 from ecologits.log import logger
 from ecologits.model_repository import ParametersMoE, models
 from ecologits.status_messages import ErrorMessage, ModelNotRegisteredError, WarningMessage, ZoneNotRegisteredError
@@ -28,6 +28,7 @@ class ImpactsOutput(BaseModel):
     gwp: Optional[GWP] = None
     adpe: Optional[ADPe] = None
     pe: Optional[PE] = None
+    water: Optional[Water] = None
     usage: Optional[Usage] = None
     embodied: Optional[Embodied] = None
     warnings: Optional[list[WarningMessage]] = None
@@ -95,7 +96,9 @@ def llm_impacts(
     if_electricity_mix_adpe=electricity_mix.adpe
     if_electricity_mix_pe=electricity_mix.pe
     if_electricity_mix_gwp=electricity_mix.gwp
+    if_electricity_mix_wcf=electricity_mix.wcf
     impacts = compute_llm_impacts(
+        provider=provider,
         model_active_parameter_count=model_active_params,
         model_total_parameter_count=model_total_params,
         output_token_count=output_token_count,
@@ -103,12 +106,19 @@ def llm_impacts(
         if_electricity_mix_adpe=if_electricity_mix_adpe,
         if_electricity_mix_pe=if_electricity_mix_pe,
         if_electricity_mix_gwp=if_electricity_mix_gwp,
+        if_electricity_mix_wcf=if_electricity_mix_wcf
     )
     impacts = ImpactsOutput.model_validate(impacts.model_dump())
+
+    
+
 
     if model.has_warnings:
         for w in model.warnings:
             logger.warning_once(str(w))
             impacts.add_warning(w)
+    #TODO : n'oublie pas d'ajouter l'avertissement si une région sans donnés sur WCF a été choisie.
 
     return impacts
+
+
