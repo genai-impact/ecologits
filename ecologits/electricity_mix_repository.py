b6416f4f6c625dc4081c1591ab1845e27875f644
Samuel Rince <s@rince.me>
2025-09-30 15:00:04 +0200

    fix: remove unused logic about missing wue values for the electricity mix

--- ecologits/electricity_mix_repository.py
+++ ecologits/electricity_mix_repository.py
@@ -34,16 +34,7 @@ class ElectricityMixRepository:
 
     def find_electricity_mix(self, zone: str) -> Optional[ElectricityMix]:
         for electricity_mix in self.__electricity_mixes:
-            if electricity_mix.zone == "WOR":
-                wue_wor_value_record = float(electricity_mix.wue)
-
             if electricity_mix.zone == zone:
-                if electricity_mix.wue == WUE_MISSING_VALUE:
-                    logger.warning_once(f"Local wue data on {zone} not found. Using world average instead.")
-                    electricity_mix.wue = float(wue_wor_value_record)
-                else:
-                    electricity_mix.wue = float(electricity_mix.wue)
-
                 return electricity_mix
         return None
 
@@ -57,9 +48,6 @@ class ElectricityMixRepository:
         with open(filepath) as fd:
             csv = DictReader(fd)
             for row in csv:
-                if row["wue"] == "":
-                    row["wue"] = WUE_MISSING_VALUE #not using 0 because it
-                    #conflicts with test_create_electricity_mix_repository_from_scratch
                 electricity_mixes.append(
                     ElectricityMix(
                         zone=row["name"],
@@ -70,6 +58,5 @@ class ElectricityMixRepository:
                     )
                 )
         return cls(electricity_mixes)
-WUE_MISSING_VALUE = -20
 
 electricity_mixes = ElectricityMixRepository.from_csv()
