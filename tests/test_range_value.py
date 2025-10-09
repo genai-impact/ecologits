17859de7cb9c91933a5de771c74f8e80d225bc26
Samuel Rince <s@rince.me>
2025-10-09 18:50:12 +0200

    test: update format test

--- tests/test_range_value.py
+++ tests/test_range_value.py
@@ -6,8 +6,8 @@ from ecologits.utils.range_value import RangeValue
 
 
 def test_range_formats_ok():
-    range = RangeValue(min =0.00000006, max=0.00008)
-    expected = f"{range.min:.2f} to {range.max:.2f}"
+    range = RangeValue(min=0.00000006, max=0.00008)
+    expected = f"{range.mean:.2f} [{range.min:.2f} - {range.max:.2f}]"
     assert f"{range:.2f}" == expected
 
 
